0c14eb725504b4c93f672d341cba3a56d57244fd
akari <terrorblade047@gmail.com>
2022-04-16 03:08:15 -0700

    Solved problems upto num4

--- practice.c
+++ practice.c
@@ -28,18 +28,66 @@ int main()
     // IF the value of n % 2 == 0, then print it as an even num
     // ELSE, print it out as an odd num
 
-    int i, n;
+    // int i, n;
 
-    printf("Input a the value: ");
+    // printf("Input a the value: ");
+    // scanf("%d", &n);
+
+    // i = n % 2;
+    // if(i == 0){
+    //     printf("%d is an even integer!", n);
+    // }
+    // else{
+    //     printf("%d is an odd integer...", n);
+    // }
+
+
+    // 4. Write a C program to check whether the given value is positive or not
+
+    // Prompt user for input
+    int n;
+
+    printf("\nInput a value: ");
     scanf("%d", &n);
 
-    i = n % 2;
-    if(i == 0){
-        printf("%d is an even integer!", n);
+    // IF n is >= 0, print value as a positive num
+    if(n >= 0){
+        printf("%d is a positive number", n);
     }
+    // ELSE print it as negative
     else{
-        printf("%d is an odd integer...", n);
+        printf("%d is a negative number.", n);
     }
+
+
+
+
+
+
+
+
+
+
+    /* 5. Write a C program to read the age of a candidate and 
+    determine whether it is eligible for casting its own vote.*/
+
+    // Prompt user for age
+    // IF user's age > 18, then accept its eligibility
+    // ELSE, decline user's eligibility for voting.
+
+    // int age;
+    // printf("\n");
+    // printf("Enter your age: ");
+    // scanf("%d", &age);
+
+    // if(age >= 18){
+    //     printf("Congratulations! You are eligible to vote!");
+    // }
+    // else{
+    //     printf("You are not eligible to vote...\n");
+    //     printf("You would be eligible to vote after turning 18.", age);
+
+    // }
 
     return 0;
 }
